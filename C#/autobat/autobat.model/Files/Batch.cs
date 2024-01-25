0b0252adc969a937d9b0da9e7b88f60abe0c1f98
taehyeon_ryu <taehyeon.ryu@changshininc.com>
2024-01-25 14:21:37 +0900

    Fix : Batch.cs

--- C#/autobat/autobat.model/Files/Batch.cs
+++ C#/autobat/autobat.model/Files/Batch.cs
@@ -5,7 +5,7 @@ namespace autobat.model.Files
     public class Batch : BaseModel{
        public string Name { get;  set; } = string.Empty;
         public FileType FileType { get;  set; } = FileType.bat;
-        
+
         public string Command { get;  set; } = string.Empty;
 
         public string FullName {get; set;} = string.Empty;
